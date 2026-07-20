24e4ac825d26ac5736d18175844c6475c73eab6d
mohamed badr <105178022+mohamed-badr7@users.noreply.github.com>
2026-07-20 14:31:31 +0300

    Add files via upload

---
+++ LMS.Client/Views/Shared/Partials/_NavbarStudent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Web.Views.Shared.Partials
+{
+    public class _NavbarStudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LMS.Client/Views/Shared/Partials/_NavbarTeacher.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LMS.Web.Views.Shared.Partials
+{
+    public class _NavbarTeacherModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
