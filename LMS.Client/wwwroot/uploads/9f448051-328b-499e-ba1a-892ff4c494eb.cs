1d1c13358218530ac653b8105ca948093f7e0e1c
mohamed badr <105178022+mohamed-badr7@users.noreply.github.com>
2026-07-20 14:41:22 +0300

    Add files via upload

---
+++ LMS.Client/wwwroot/uploads/9f448051-328b-499e-ba1a-892ff4c494eb.cs
@@ -0,0 +1,37 @@
+﻿using LMS.Entities.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Web.ViewModels;
+
+public class StudentProfileViewModel
+{
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+    [Required]
+    [EnumDataType(typeof(Gender))]
+    public Gender Gender { get; set; }
+    [Required]
+    [StringLength(20)]
+    public required string EmergencyContact { get; set; }
+    [Required]
+    public DateTime AdmissionDate { get; set; } = DateTime.UtcNow;
+    [Required]
+    [StringLength(255)]
+    public required string StudentNumber { get; set; }
+    [Required]
+    [Display(Name = "Full Name")]
+    public required string FullName { get; set; }
+    [Required]
+    public required string Address { get; set; }
+    [Display(Name = "Profile Picture")]
+    public string? ProfilePictureURL { get; set; }
+    [Required]
+    [EnumDataType(typeof(GradeLevel))]
+    public GradeLevel? GradeLevel { get; set; }
+    [Required]
+    [StringLength(255)]
+    public required string ClassNumber { get; set; }
+    [Display(Name = "Bus Number")]
+    public string BusNumber { get; set; }
+
+}
